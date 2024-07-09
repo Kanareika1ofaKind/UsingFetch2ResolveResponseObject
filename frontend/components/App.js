0ea83f0e31ff7aad20582809e6e9be1c6ee27f98
Sydney <proteimus@gmail.com>
2024-07-09 11:53:25 -0500

    Status Request

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function App() {
+  useEffect(() => {
+    function getCats() {
+      fetch('http://localhost:3003/api/cats')
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(`Ouch ${res.status}`) 
+        }
+        const type = res.headers.get('Content-Type')
+        if (type.includes('application/json')) {
+          debugger
+        }
+      })
+      .catch(err => {
+        console.log(`Problem GETing cats`, err); 
+        debugger
+      })
+    }
+    getCats()
+  }, []) 
   return (
     <div>
-      <h2>Hello, World!</h2>
+      <h2>Hello, Cats!</h2>
     </div>
   )
 }
